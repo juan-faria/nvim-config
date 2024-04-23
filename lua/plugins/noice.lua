794aaf354cf83774802b9a94cbcf861c82809a65
Juan Faria <juan_acf@outlook.com>
2024-04-23 03:12:02 +0000

    Revert messages to noice.

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,36 +1,28 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"rcarriga/nvim-notify",
-	},
-	config = function()
-		require("noice").setup({
-			lsp = {
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-				},
-			},
-			presets = {
-				bottom_search = true,
-				command_palette = true,
-				long_message_to_split = true,
-				inc_rename = false,
-			},
-			cmdline = {
-				enabled = true,
-				view = "cmdline",
-			},
-			messages = {
-				enabled = true, -- enables the Noice messages UI
-				view = "messages", -- default view for messages
-				view_error = "notify", -- view for errors
-				view_warn = "notify", -- view for warnings
-				view_history = "messages", -- view for :messages
-				view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
-			},
-		})
-	end,
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  config = function()
+    require("noice").setup({
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = false,
+      },
+      cmdline = {
+        enabled = true,
+        view = "cmdline",
+      },
+    })
+  end,
 }
