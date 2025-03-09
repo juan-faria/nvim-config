074a353ce007cce8e3d5b48210c539b85ab5a78a
Juan Faria <juan_acf@outlook.com>
2025-03-09 17:56:15 +0000

    Making new version work on iPad.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,16 +1,19 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("lualine").setup({
-      sections = {
-        lualine_a = {
-          {
-            'filename',
-            path = 1,
-          }
-        }
-      }
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			sections = {
+				lualine_a = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+			},
+			options = {
+				theme = "catppuccin",
+			},
+		})
+	end,
 }
